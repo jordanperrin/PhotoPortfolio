b5bfcc75dbd3c0e570e4a7101fa52075613effe2
jordanperrin <93149223+jordanperrin@users.noreply.github.com>
2023-10-30 15:37:54 -0400

    Rearranged component layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,11 @@
 import React from 'react';
 import './App.css';
-import img1 from './images/post1-7.jpg';
-
-let prevVal;
-function getRandomNumber(){
-  let newVal =  Math.floor(Math.random() * 16) + 1;
-  while(newVal === prevVal || newVal === prevVal+1 || newVal === prevVal-1 ){
-    newVal =  Math.floor(Math.random() * 16) + 1;
-  }
-  return newVal;
-}
+import Film from './film'
 
 const App = () => {
-  const squareDivsLeft = [];
-  for (let i = 0; i < 6; i++) {
-    let string = "square" + getRandomNumber();
-    squareDivsLeft.push(
-      <div id={string} className='sqaure'>
-        <span className="invisible"> Placeholder</span>
-      </div>
-    );
-  }
-
-  const squareDivsRight = [];
-  for (let i = 0; i < 6; i++) {
-    let string = "square" + getRandomNumber();
-    squareDivsRight.push(
-      <div id={string} className='sqaure'>
-        <span className="invisible"> Placeholder</span>
-      </div>
-    );
-  }
 
   return (
-    <div className="mainFilm">
-      <div className="perfs">
-        <div id="leftPerf">
-          {squareDivsLeft}
-        </div>
-      </div>
-      <div className="imgContainer">
-          <img src={img1} />
-          <div className="block"></div>
-      </div>
-      <div className="perfs">
-        <div id="rightPerf">
-          {squareDivsRight}
-        </div>
-      </div>
-    </div>
+    <Film />
   );
 };
 
---
+++ client/src/film.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './film.css';
+import img1 from './images/post1-7.jpg';
+
+let prevVal;
+const getRandomNumber = ()=>{
+  let newVal =  Math.floor(Math.random() * 16) + 1;
+  while(newVal === prevVal ){
+    newVal =  Math.floor(Math.random() * 16) + 1;
+  }
+  return newVal;
+}
+
+const generatePerfs = () =>{
+  const listOfPerfs = [];
+  for (let i = 0; i < 6; i++) {
+    let string = "square" + getRandomNumber();
+    listOfPerfs.push(
+      <div id={string} className='sqaure'>
+        <span className="invisible"> Placeholder</span>
+      </div>
+    );
+  }
+   return listOfPerfs;
+}  
+
+const Film = () => {
+
+  return (
+    <div className="mainFilm">
+      <div className="perfs">
+        <div id="leftPerf">
+          {generatePerfs()}
+        </div>
+      </div>
+      <div className="imgContainer">
+          <img src={img1} />
+          <div className="block"></div>
+      </div>
+      <div className="perfs">
+        <div id="rightPerf">
+          {generatePerfs()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Film;
