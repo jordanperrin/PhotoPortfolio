64fdc98fa7dfc9f92cc8b734193db591a8e753ef
jordanperrin <93149223+jordanperrin@users.noreply.github.com>
2023-11-01 11:41:11 -0400

    Refactored code to fit for mobile

--- client/src/components/BWFIlmReelHoriz/film.js
+++ client/src/components/BWFIlmReelHoriz/film.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './film.css';
 import img1 from '../../images/post1-7.jpg';
+import img2 from '../../images/post1-4.jpg';
+import img3 from '../../images/post1-2.jpg';
+import img4 from '../../images/post2-8.jpg';
+import img5 from '../../images/post2-10.jpg';
 
 let prevVal;
 
@@ -14,7 +18,7 @@ const getRandomNumber = ()=>{
 
 const generatePerfs = () =>{
   const listOfPerfs = [];
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 22; i++) {
     let string = "square" + getRandomNumber();
     listOfPerfs.push(
       <div id={string} className='sqaure'>
@@ -34,14 +38,33 @@ const Film = () => {
         </div>
       </div>
       <div className="imgContainer">
-          <img src={img1} />
-          <div className="block"></div>
+          <div className="inside-img" id="first-img">
+            <img src={img1} />
+            <div className="block"></div>
+          </div>
+          <div className="inside-img">
+            <img src={img2} />
+            <div className="block"></div>
+          </div>
+          <div className="inside-img">
+            <img src={img3} />
+            <div className="block"></div>
+          </div>
+          <div className="inside-img">
+            <img src={img4} />
+            <div className="block"></div>
+          </div>
+          <div className="inside-img">
+            <img src={img5} />
+            <div className="block"></div>
+          </div>
       </div>
       <div className="perfs">
         <div id="rightPerf">
           {generatePerfs()}
         </div>
       </div>
+      
     </div>
   );
 };
