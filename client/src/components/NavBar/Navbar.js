eec4b1e08637040aea56828598e840a2a852b7f6
jordanperrin <93149223+jordanperrin@users.noreply.github.com>
2023-11-22 12:04:33 -0500

    finish UI for nav, need to link pages

--- client/src/components/NavBar/Navbar.js
+++ client/src/components/NavBar/Navbar.js
@@ -41,6 +41,17 @@ const Navbar = () =>{
                             <IoMdClose className='x' onClick={showSidebar} />
                         )}
                 </div>
+
+                <div className='film-burn-nav'></div>
+
+                <svg >
+                    <filter id="wavy-nav">
+                    <feTurbulence x="0" y="0" baseFrequency="0.109" numOctaves="4" seed="2">
+                        <animate attributeName="baseFrequency" dur="80s" values="0.02;0.005;0.02" repeatCount="indefinite" />
+                    </feTurbulence>
+                    <feDisplacementMap in="SourceGraphic" scale="25" />
+                    </filter>
+                 </svg> 
             </nav>
 
             <nav className=  {sidebar ?  'side active': 'side'}>
