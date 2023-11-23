3e994132610b28459a3547968d4ac0ea4d0df7cc
jordanperrin <93149223+jordanperrin@users.noreply.github.com>
2023-11-22 19:33:10 -0500

    added fade-in-out for ig/contact icons

--- client/src/components/BWFIlmReelHoriz/film.js
+++ client/src/components/BWFIlmReelHoriz/film.js
@@ -4,11 +4,13 @@ import { LuInstagram as IgIcon} from "react-icons/lu";
 import { LuMail } from "react-icons/lu";
 import {Link} from 'react-router-dom';
 import './film.css';
+//TODO: need to change the way we import these images -> use an 
 import img1 from '../../images/post1-7.jpg';
 import img2 from '../../images/post1-4.jpg';
 import img3 from '../../images/post1-2.jpg';
 import img4 from '../../images/post2-8.jpg';
 import img5 from '../../images/post2-10.jpg';
+import clr from '../../images/color1.jpeg'
 
 let prevVal;
 const getRandomNumber = ()=>{
@@ -19,6 +21,7 @@ const getRandomNumber = ()=>{
   return newVal;
 }
 
+//TODO: need to use Hooks for this
 const generatePerfs = () =>{
   const listOfPerfs = [];
   for (let i = 0; i < 26; i++) {
@@ -33,20 +36,21 @@ const generatePerfs = () =>{
 }  
 
 const Film = () => {
-  const myRef = useRef();
-  const [myElementIsVisible, setmyElementIsVisible] = useState();
+  const burnRef = useRef();
+  const [burnDivVisible, setburnDivVisible] = useState();
+  const [btmIconsVisible, setbtmIconsVisible] = useState();
 
   useEffect(() =>{
      const observer = new IntersectionObserver((entries)=>{ //callback function that is going to be fired every time visibility of that observed element
         const entry = entries[0];
-        setmyElementIsVisible(entry.isIntersecting); //uses the useState() hook
+        setburnDivVisible(entry.isIntersecting); //uses the useState() hook
         //  console.log("entry", entry);
       });
  
-     observer.observe(myRef.current )
-  }, [])
+     observer.observe(burnRef.current )
+  }, [])//empty array as a dependency will only run once when component is initialized
 
-  //empty array as a dependency 
+  
   return (
     <div className="film-container"> 
       <div className="mainFilm">
@@ -91,8 +95,8 @@ const Film = () => {
       </div>
 
       <div className = "burn-container">
-        <div ref={myRef} className={` ${myElementIsVisible ? 'film-burn' : "off-screen"}`}></div>
-        <div className='ig-container'>
+        <div ref={burnRef} className={` ${burnDivVisible ? 'film-burn' : "off-screen"}`}></div>
+        <div className={`${burnDivVisible ? 'ig-container show' : 'ig-container'}`}>
           <ul className='contact-list'>
             <li>
               <Link to='https://www.instagram.com/cinema.tif/'>
@@ -107,7 +111,6 @@ const Film = () => {
           </ul>
         </div>
       </div>
-
 
       <svg >
         <filter id="wavy">
